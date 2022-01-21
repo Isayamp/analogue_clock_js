c0ff2702e9358db4d6bb7377f4dc656e1c220eb1
Pombo Isaya M'lela <isayamlelapombo@gmail.com>
2022-01-21 17:38:47 +0200

    Création fonction pour tourner les aigguilles

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,7 @@
 const hourHand = document.getElementById('hour-hand');
 const minuteHand = document.getElementById('minute-hand');
 const secondHand = document.getElementById('second-hand');
+
+function rotateClockHands() {
+    
+}

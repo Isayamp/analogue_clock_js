4c06228d37f9c684c4f559d62371b3da134f1864
Pombo Isaya M'lela <isayamlelapombo@gmail.com>
2022-01-21 17:34:16 +0200

    Récuperer les aiguilles dans des constantes

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// Obtient les éléments des aiguilles de l'horloge
+const hourHand = document.getElementById('hour-hand');
+const minuteHand = document.getElementById('minute-hand');
+const secondHand = document.getElementById('second-hand');

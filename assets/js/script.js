a381b6f28b690ce24e5bb87e8a7f4fbb8abb0b07
Pombo Isaya M'lela <isayamlelapombo@gmail.com>
2022-01-21 17:49:49 +0200

    Transformations CSS; Rotation des aiguilles

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,4 +14,9 @@ function rotateClockHands() {
     const hourRotation = (hours * 30) + (minutes * 0.5);
     const minuteRotation = (minutes * 6) + (seconds * 0.1);
     const secondRotation = seconds * 6;
+
+    // Applique la rotation aux aiguilles en utilisant des transformations CSS
+    hourHand.style.transform = `rotate(${hourRotation}deg)`;
+    minuteHand.style.transform = `rotate(${minuteRotation}deg)`;
+    secondHand.style.transform = `rotate(${secondRotation}deg)`;
 }

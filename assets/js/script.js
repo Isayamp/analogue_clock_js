6d0b956b1fc6aad8969e870d80f26b4b9047c661
Pombo Isaya M'lela <isayamlelapombo@gmail.com>
2022-01-21 17:40:41 +0200

    Récuperation de la date et l'heure du système

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,9 @@ const minuteHand = document.getElementById('minute-hand');
 const secondHand = document.getElementById('second-hand');
 
 function rotateClockHands() {
-    
+    // Obtient la date et l'heure actuelles
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
 }
